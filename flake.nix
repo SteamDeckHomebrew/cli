c2d7a4a20075d83ffeb3b9bc7e931ca572f6bc2a
Sky Leite <sky@leite.dev>
2023-01-26 20:39:26 -0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Decky CLI!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-analyzer
+            cargo
+            cargo-edit
+            gcc
+            openssl
+            pkg-config
+            (rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override { extensions = [ "rust-src" ]; }))
+          ];
+        };
+      });
+}
